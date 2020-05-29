857e416c3994cbaca572243dff332c71ea96150c
André Rosa <andre.reddrain@gmail.com>
2020-05-29 17:39:26 +0100

    Comment all previous created Routes.

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/about', function () {
-    return "Hi about page";
-});
+// Route::get('/about', function () {
+//     return "Hi about page";
+// });
 
-Route::get('/contact', function () {
-    return "Hi I am contact";
-});
+// Route::get('/contact', function () {
+//     return "Hi I am contact";
+// });
 
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return "This is post nº $id, $name";
-});
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return "This is post nº $id, $name";
+// });
 
-Route::get('admin/posts/example', array('as' => 'admin.home' , function () {
-    $url = route('admin.home');
+// Route::get('admin/posts/example', array('as' => 'admin.home' , function () {
+//     $url = route('admin.home');
 
-    return "this url is $url";
-}));
+//     return "this url is $url";
+// }));
 
 // Route::get('/post/{id}', 'PostsController@index');
 
